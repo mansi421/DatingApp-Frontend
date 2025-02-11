fa18c39a96ef3dbe5718398823ee88a667a8c195
mansi421 <mansi.jain.cs.2018@miet.ac.in>
2025-02-11 22:53:12 +0530

    1.added functionality to Login/register user. 2. new syntax of input/output decorator. 3. new syntax for structural directive.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,29 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  http = inject(HttpClient);
+  private accoutService = inject(AccountService);
   title = 'client';
-  users : any
 
   ngOnInit() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.error(error),
-      complete: () => console.log('complete')
-    })
+    this.setCurrentUser();
   }
+
+  setCurrentUser() {
+    let userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accoutService.currentUser.set(user);
+  }
+
 }
