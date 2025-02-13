dcd334901de4498770a189e5afa06ec9a4ba37e3
mansi421 <mansi.jain.cs.2018@miet.ac.in>
2025-02-13 22:10:54 +0530

    Adding Routes and Route guards

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,28 +2,35 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule,RouterLink,RouterLinkActive,TitleCasePipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   model: any = {}
-  accountService = inject(AccountService)
+  accountService = inject(AccountService);
+  private router = inject(Router);
+  private toaster = inject(ToastrService);
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response);
+        this.router.navigateByUrl('/members');
       },
-      error: error => console.error(error),
+      error: error => this.toaster.error(error.error),
     })
   }
 
   logout() {
     this.accountService.logout();   
+    this.router.navigateByUrl('/');
+
   }
 }
